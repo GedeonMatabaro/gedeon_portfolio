7b1c75b72768ba4153cb516342bb82b80cce3f3b
GedeonMatabaro <mugishomatgedeon@gmail.com>
2023-06-07 04:14:18 +0530

    My Firt Commit of my portfolio

---
+++ src/data/headerData.js
@@ -0,0 +1,9 @@
+
+export const headerData = {
+    name: 'Gedeon MUGISHO MATABARO',
+    title: "Hello! I am",
+    desciption: "Hello! I am GEDEON MUGISHO MATABARO, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about utilizing technology to drive positive change in the world.",
+    image: 'https://i.ibb.co/2kMmXzP/passport-gdm.jpg',
+    imagebw: 'https://i.ibb.co/fQCvSz9/passport-gdm.jpg',
+    resumePdf: 'https://drive.google.com/file/d/1HiNmcOuFFViPKd231Pi3Gt3uqRojj6F2/view?usp=sharing'
+}
---
+++ src/data/projectsData.js
@@ -0,0 +1,50 @@
+import eight from '../assets/svg/projects/eight.svg'
+import seven from '../assets/svg/projects/seven.svg'
+import six from '../assets/svg/projects/six.svg'
+
+
+export const projectsData = [
+    {
+        id: 1,
+        projectName: 'KIITNERDS',
+        projectDesc: 'This project Online Learning System Management.',
+        tags: ['React', 'SAAS', 'React Bootstrap'],
+        code: 'https://github.com/GedeonMatabaro/KNERDS/tree/master',
+        demo: 'https://gdi1wr-5173.csb.app/',
+        image: six
+    },
+    {
+        id: 2,
+        projectName: 'KAZEABROAD',
+        projectDesc: 'This is the first website I designed for KazeAbroad, an Agency based on education.',
+        tags: ['React', 'Express', 'Material Ui'],
+        code: '#',
+        demo: 'https://kazearboad.com/',
+        image: seven
+    },
+    {
+        id: 3,
+        projectName: 'Great Dream Academy',
+        projectDesc: 'This project about a educational agency.',
+        tags: ['HTML', 'CSS', 'JavaScript'],
+        code: '#',
+        demo: 'https://greatdreamacademy.com/',
+        image: eight
+    }
+]
+
+
+// Do not remove any fields.
+// Leave it blank instead as shown below
+
+/* 
+{
+    id: 1,
+    projectName: 'Car Pooling System',
+    projectDesc: '',
+    tags: ['Flutter', 'React'],
+    code: '',
+    demo: '',
+    image: ''
+}, 
+*/
