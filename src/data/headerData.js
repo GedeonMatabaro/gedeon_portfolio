c4f95e480060357aa37337eb7a0c80e089fd7620
GedeonMatabaro <mugishomatgedeon@gmail.com>
2023-06-07 05:03:54 +0530

    Correcting the project data

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,7 +1,7 @@
 
 export const headerData = {
     name: 'Gedeon MUGISHO MATABARO',
-    title: "Hello!",
+    title: "",
     desciption: "I am GEDEON MUGISHO MATABARO, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about utilizing technology to drive positive change in the world.",
     image: 'https://i.ibb.co/2kMmXzP/passport-gdm.jpg',
     imagebw: 'https://i.ibb.co/fQCvSz9/passport-gdm.jpg',
